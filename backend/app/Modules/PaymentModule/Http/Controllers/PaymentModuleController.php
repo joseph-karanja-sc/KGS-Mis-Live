cc5412f813bf51554b6e36ccb5d7fbfb3549318c
pndungu-dev <peter.ndungu@softclans.co.ke>
2026-03-31 20:17:42 +0300

    Fix ...

--- backend/app/Modules/PaymentModule/Http/Controllers/PaymentModuleController.php
+++ backend/app/Modules/PaymentModule/Http/Controllers/PaymentModuleController.php
@@ -5387,10 +5387,10 @@ class PaymentModuleController extends BaseController
                 // ->join('payment_verificationbatch as t6', 't5.batch_id', '=', 't6.id')
                 ->leftJoin('beneficiary_school_statuses as t7', 't5.beneficiary_schoolstatus_id', '=', 't7.id')
                 // ->leftJoin('beneficiary_payment_records as t8', 't5.id', '=', 't8.enrollment_id')
-                ->leftJoin('beneficiary_payment_records as t8', function ($join) {
-                    $join->on('t5.id', '=', 't8.enrollment_id')
-                        ->on('t8.payment_request_id', '!=', 55); 
-                })
+                // ->leftJoin('beneficiary_payment_records as t8', function ($join) {
+                //     $join->on('t5.id', '=', 't8.enrollment_id')
+                //         ->on('t8.payment_request_id', '!=', 55); 
+                // })
 
                 
                 //->join('school_terms as t9', 't5.term_id', '=', 't9.id')
